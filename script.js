7cf46b703b6edc4a615974855794e888707ea187
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-22 15:14:55 +0200

    Correct and finish the 2nd exercise

--- script.js
+++ script.js
@@ -14,9 +14,29 @@ Dichiariamo chi ha vinto.
 */
 
 const userChoice = prompt('Scegli pari o dispari');
-const userNumber = prompt(parseInt('Scegli un numero fra 1 e 5'));
-const casualNumber = generaNumeroCasuale();
+console.log(userChoice);
 
-function generaNumeroCasuale(){
-    
+const userNumber = parseInt(prompt('Scegli un numero fra 1 e 5'));
+console.log(userNumber);
+
+const casualNumber = (Math.floor(Math.random() * 5) + 1);
+console.log(casualNumber);
+
+if (userNumber % 2 === 0) {
+    console.log ('Il numero che hai scelto è pari')
+} else {
+    console.log ('Il numero che hai scelto è dispari')
 }
+
+function vincitore (userChoice, userNumber, casualNumber){
+    const sum = userNumber + casualNumber;
+
+    if ((sum % 2 === 0 && userChoice === 'pari')||(sum % 2 !=0 && userChoice === 'dispari')) {
+        return 'Hai vinto!';
+    } else {
+        return 'Hai perso';
+    }
+}
+
+const risultato = vincitore(userChoice, userNumber, casualNumber);
+console.log(risultato);
